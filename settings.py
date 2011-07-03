6c5ff649f9e6d1e5b2f9eb0a394bc9a8d992d606
Rafiq Rahim <bizkut@gmail.com>
2011-07-03 21:02:12 +0900

    Changed media location in setting.py

--- settings.py
+++ settings.py
@@ -45,7 +45,7 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = 'C:/Users/ruffiQ/photoframe/pictures/'
+MEDIA_ROOT = 'pictures/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
--- view.py
+++ view.py
@@ -6,5 +6,5 @@ import glob
 
 def show_pic(request):
     piclist = glob.glob(os.path.join(os.path.dirname(__file__), 'pictures') + '/*.jpg')
-    pic = piclist[0].rsplit('\\')[5]
+    pic = piclist[0].rsplit('\\')[-1]
     return render_to_response('base.html', locals())
