3ae0453b81d14ba82422e55064a4ef345a29ca90
unknown <rafiq@.(none)>
2011-07-06 19:17:51 +0900

    hello django!

--- view.py
+++ view.py
@@ -5,6 +5,6 @@ from django.shortcuts import render_to_response
 import glob
 
 def show_pic(request):
-    piclist = glob.glob(os.path.join(os.path.dirname(__file__), 'pictures') + '/*.jpg')
-    pic = piclist[0].rsplit('\\')[-1]
+    piclist = glob.glob(os.path.join(os.path.dirname(__file__), u'pictures') + u'/*.jpg')
+    pic = piclist[0].rsplit('\\')[-1].encode('utf8')
     return render_to_response('base.html', locals())
