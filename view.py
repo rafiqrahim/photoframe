84d1e8d80c6282a1e1a3e0108bdc5f71a3e5277a
unknown <ruffiQ@.(none)>
2011-07-09 13:59:50 +0900

    simple picture viewer

--- view.py
+++ view.py
@@ -5,9 +5,18 @@ from django.shortcuts import render_to_response
 import glob
 
 def show_pic(request):
-    piclist = glob.glob(os.path.join(os.path.dirname(__file__), u'media/assets/images') + u'/*.jpg')
-    pic = '/'.join(piclist[0].replace('/','\\').rsplit('\\')[-4:]).encode('utf8')
+
     return render_to_response('base.html', locals())
 
 def slide(request):
+    piclist = glob.glob(os.path.join(os.path.dirname(__file__), u'media/assets/images') + u'/*.jpg')
+    pic = '/'.join(piclist[0].replace('/','\\').rsplit('\\')[-4:]).encode('utf8')
+    name = '/'.join(piclist[0].replace('/','\\').rsplit('\\')[-1:]).rsplit('.')[0].encode('utf8')
+    sec=''
+    for i in range(3):
+        a = glob.glob(os.path.join(os.path.dirname(__file__), u'media/assets/images') + u'/*.jpg')
+
+        b = '/'.join(piclist[i].replace('/','\\').rsplit('\\')[-4:]).encode('utf8')
+        c = '/'.join(piclist[i].replace('/','\\').rsplit('\\')[-1:]).rsplit('.')[0].encode('utf8')
+        sec = sec + "<section><h3>" + c + "</h3><img src='" + b + "'></section>"
     return render_to_response('index.html',locals())
